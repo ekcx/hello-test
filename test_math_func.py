ccfd8a9fadde9071129074a66cadc3ba1e4e503a
ekcx <ekcx92@gmail.com>
2023-05-23 16:25:46 +0300

    Test Files were added.

---
+++ test_math_func.py
@@ -0,0 +1,37 @@
+import math_func
+
+def test_add():
+    assert math_func.add(7) == 9
+
+def test_product():
+    assert math_func.product(5, 5) == 25
+    assert math_func.product(5) == 10
+    assert math_func.product(7) == 14
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
